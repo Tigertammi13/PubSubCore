d0557f3715f9ec780f57c8477d5615bdaddade0e
Alexandru-Mihai Suciu <alexandru.suciu@student.manchester.ac.uk>
2020-11-28 21:19:40 +0200

    Create basic server and service

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from flask import request
+from queue import Queue
+import socket
+import threading
 name = "PubSubCore"
 app = Flask(name)
 
 
-@app.route('/',methods=['GET'])
+
+
+@app.route('/', methods=['GET'])
 def homepage():
     result = ""
     with open("./frontend/welcome.html") as file:
         return file.read()
 
-@app.route('/',methods=['POST'])
+@app.route('/', methods=['POST'])
 def page2():
     req = request.form
-    print(req)
-    return  "dufghdfug"
+    print(req['sum_of'].split())
+    return "asdsad"
+
 
 if name == 'main':
     app.run(use_reloader=True, debug=True)
---
+++ backend/test_microservice.py
@@ -0,0 +1,33 @@
+import socket
+import threading
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 5555))
+id = "Calc#1"
+
+
+def recieve():
+    while True:
+        try:
+            message = client.recv(1024).decode("ascii")
+            if message == "WHO":
+                client.send(id.encode("ascii"))
+            else:
+                print(message.decode("ascii"))
+        except:
+            print("An error occured".encode("ascii"))
+            client.close()
+            break
+
+
+def write():
+    while True:
+        mesage = f'{input()}'
+        client.send(mesage.encode("ascii"))
+
+
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
